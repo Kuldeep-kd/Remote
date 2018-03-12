0e20e31f3db4439d25249c8aa85c3f1b1ef04399
Kuldeep Thakre <Kuldeep Thakre>
2018-03-13 01:50:36 +0530

    Add project files.

---
+++ PresentationSlider/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Windows.Forms;
+using GodSharp.Sockets;
+
+
+
+namespace PresentationSlider
+{
+    class Program 
+    {
+        
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            SocketServer server = new SocketServer(port: 7788)
+            {
+                OnConnected = (sender) =>
+                {
+                    Console.WriteLine($"Client {sender.RemoteEndPoint.ToString()} connected");
+                }
+            };
+
+            server.OnData = (sender, data) =>
+            {
+                //get client data
+                string message = server.Encoding.GetString(data, 0, data.Length);
+                Console.WriteLine($"server received data from {sender.RemoteEndPoint}: {message}");
+
+                //message = "server repley " + message;
+                message = random.Next(100000000, 999999999).ToString();
+                sender.Send(message);
+
+                Console.WriteLine($"server send data to {sender.RemoteEndPoint}: {message}");
+            };
+
+            server.Listen();
+            server.Start();
+
+            Console.ReadKey();
+
+
+        }
+
+        private static void keypress()
+        {
+            try
+            {
+               
+                SendKeys.SendWait("X");
+                SendKeys.SendWait("Winkey");
+
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                
+            }
+            
+        }
+
+
+      
+
+
+}
+}
---
+++ client/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using GodSharp.Sockets;
+
+namespace client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ReadKey();
+
+            SocketClient client = new SocketClient("192.168.43.2", 7788);
+
+            client.OnData = (sender, data) =>
+            {
+                //get server data
+                string message = client.Encoding.GetString(data, 0, data.Length);
+                Console.WriteLine($"client received data from {sender.RemoteEndPoint.ToString()}: {message}");
+            };
+
+            client.Connect();
+            client.Start();
+
+            string msg = Console.ReadLine();
+
+
+            while (msg.ToLower() != "q")
+            {
+                client.Sender.Send(msg);
+                Console.WriteLine($"client send data to {client.RemoteEndPoint.ToString()}: {msg}");
+                msg = Console.ReadLine();
+            }
+        }
+    }
+}
